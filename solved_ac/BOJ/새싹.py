820164e4bc02025f11f91d456198187c1ff2d109
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-03 15:00:38 +0900

    0803

---
+++ solved_ac/BOJ/새싹.py
@@ -0,0 +1,16 @@
+#          ,r'"7
+# r`-_   ,'  ,/
+#  \. ". L_r'
+#    `~\/
+#       |
+#       |
+
+# "Hello"
+# System.out.println("\"Hello\"");
+
+print("          ,r\'\"7")
+print(" r`-_   ,'  ,/")
+print("  \. \". L_r\'")
+print("    `~\/")
+print("       |")
+print("       |")
