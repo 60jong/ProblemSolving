706c756d3f905a1ce4bfae81d6f9162a71d67579
60jong <rudwhd515@gmail.com>
2023-08-23 10:49:53 +0900

    0823

--- solved_ac/BOJ/분할정복/비요뜨의 징검다리 건너기.py
+++ solved_ac/BOJ/분할정복/비요뜨의 징검다리 건너기.py
@@ -3,16 +3,24 @@ import sys
 input = sys.stdin.readline
 divide = 10 ** 9 + 7
 
+# def fpow(a, b):
+#     if b == 1:
+#         return a % divide
+#
+#     x = fpow(a, b // 2)
+#     if b & 1:
+#         return x * x * a % divide
+#     else:
+#         return x * x % divide
+
 def fpow(a, b):
-    if b == 1:
-        return a % divide
-
-    x = fpow(a, b // 2)
-    if b & 1:
-        return x * x * a % divide
-    else:
-        return x * x % divide
-
+    result = 1
+    while b > 0:
+        if b & 1:
+            result *= a % divide
+        a *= a % divide
+        b >>= 1
+    return result
 T = int(input())
 
 
