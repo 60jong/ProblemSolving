52b9411b884271e7f8b4428afe72690c4cec14e9
60jong <rudwhd515@gmail.com>
2023-09-03 21:01:02 +0900

    텀프로젝트 시간초과 개선

--- solved_ac/BOJ/BFS/텀프로젝트.py
+++ solved_ac/BOJ/BFS/텀프로젝트.py
@@ -1,38 +1,37 @@
 import sys
 from collections import deque
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
     N = int(input())
     remain = N
-    nums = list(map(int,input().strip().split()))
+    nums = list(map(int, input().strip().split()))
     visited = [False] * N
     q = deque()
     for i in range(N):
-        team_made = False
+        temp = dict()
+        seq = 0
+
         if not visited[i]:
-            q.append(nums[i]-1)
             visited[i] = True
-        temp = []
+            q.append(nums[i] - 1)
+            temp[i] = seq
+            seq += 1
+
         while q:
             next_i = q.popleft()
 
             # 사이클 존재
-            if next_i == i:
-                team_made = True
-                remain -= len(temp) + 1
-                for t in temp:
-                    visited[t] = True
+            if next_i in temp:
+                cycle_len = len(temp) - temp[next_i]
+                remain -= cycle_len
                 break
 
             if not visited[next_i]:
-                q.append(nums[next_i]-1)
                 visited[next_i] = True
-                temp.append(next_i)
-        if not team_made:
-            for t in temp:
-                visited[t] = False
-
+                q.append(nums[next_i] - 1)
+                temp[next_i] = seq
+                seq += 1
     print(remain)
