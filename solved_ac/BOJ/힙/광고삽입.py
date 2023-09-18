22bf70c6ab6262cffc123d41f08c4d2bcefc3441
60jong <rudwhd515@gmail.com>
2023-09-18 21:40:19 +0900

    0918

--- solved_ac/BOJ/힙/광고삽입.py
+++ solved_ac/BOJ/힙/광고삽입.py
@@ -1,5 +1,3 @@
-from heapq import heappush, heappop
-
 def to_num(time):
     a = time.split(":")
     return int(a[0]) * 3600 + int(a[1]) * 60 + int(a[2])
@@ -11,44 +9,26 @@ def to_str(num):
 
     return str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(sec).zfill(2)
 
-
 def solution(play_time, adv_time, logs):
-    num_adv_time = to_num(adv_time)
-    num_play_time = to_num(play_time)
-
-    cv_logs = [(0, 0)]
+    pts = play_time.split(':')
+    N = int(pts[0]) * 3600 + int(pts[1]) * 60 + int(pts[2]) + 1
+    accum = [0] * N
     for log in logs:
-        s, e = log.split("-")
-        cv_logs.append((to_num(s), to_num(e)))
-    cv_logs = sorted(cv_logs, key=lambda x: (x[0], x[1]))
+        start, end = log.split('-')
+        start_num, end_num = to_num(start), to_num(end)
+        accum[start_num] = accum[start_num] + 1 
+        accum[end_num] = accum[end_num] - 1
+    for i in range(N - 1):
+        accum[i + 1] = accum[i] + accum[i + 1]
 
-    accum_time = [[0, i] for i in range(len(cv_logs))]
+    nat = to_num(adv_time)
+    cur_sum = sum(accum[:nat])
+    max_sum = cur_sum
+    t_idx = 0
+    for i in range(1, (N - nat) + 1):
+        cur_sum = cur_sum - accum[i - 1] + accum[(i - 1) + nat]
+        if cur_sum > max_sum:
+            max_sum = cur_sum
+            t_idx = i
 
-    q_p, q_c = [], []
-    heappush(q_p, (num_adv_time, 0))
-    for i in range(1, len(cv_logs)):
-        # q_p
-        start_time = cv_logs[i][0]
-        # 끝난 광고 pop
-        while q_p and start_time >= q_p[0][0]:
-            heappop(q_p)
-        # 진행 중인 광고 누적
-        cp_q_p = q_p[:]
-        while cp_q_p and start_time < cp_q_p[0][0]:
-            adv_end_time, idx = heappop(cp_q_p)
-            accum_time[idx][0] = min(num_play_time, accum_time[idx][0] + adv_end_time - start_time)
-        heappush(q_p, (min(num_play_time, start_time + num_adv_time), i))
-        accum_time[i][0] = min(num_play_time, num_play_time, accum_time[i][0] + num_adv_time)
-
-        # q_c
-        cur_adv_end_time = start_time + num_adv_time
-        while q_c and start_time >= q_c[0][0]:
-            heappop(q_c)
-        cp_q_c = q_c[:]
-        while cp_q_c and cur_adv_end_time > cp_q_c[0][0]:
-            play_end_time, idx = heappop(cp_q_c)
-            accum_time[i][0] = min(num_play_time, accum_time[i][0] + cur_adv_end_time - play_end_time)
-        heappush(q_c, (min(num_play_time, cv_logs[i][1]), i))
-
-    accum_time = sorted(accum_time, key=lambda x: (-x[0], x[1]))
-    return to_str(cv_logs[accum_time[0][1]][0])
+    return to_str(t_idx)
