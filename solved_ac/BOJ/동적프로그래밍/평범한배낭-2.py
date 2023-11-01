c812102779221e82c363ed07772822b87116fe21
60jong <rudwhd515@gmail.com>
2023-11-01 16:53:02 +0900

    평벙함 배낭 dp 2d

--- solved_ac/BOJ/동적프로그래밍/평범한배낭-2.py
+++ solved_ac/BOJ/동적프로그래밍/평범한배낭-2.py
@@ -4,9 +4,14 @@ things = []
 for _ in range(N):
     things.append(tuple(map(int, input().split())))
 
-dp = [0] * (K + 1)
-for w, v in things:
-    for k in range(K, w - 1, -1):
-        dp[k] = max(dp[k - w] + v, dp[k])
+# dp[i][j] = i번째 things까지 사용해서 j 무게를 채워 얻는 최고 가치
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+for n in range(1, N + 1):
+    w, v = things[n - 1]
+    for k in range(1, K + 1):
+        if w <= k:
+            dp[n][k] = max(dp[n - 1][k - w] + v, dp[n - 1][k])
+        else:
+            dp[n][k] = dp[n - 1][k]
 
-print(dp[-1])
+print(dp[N][K])
