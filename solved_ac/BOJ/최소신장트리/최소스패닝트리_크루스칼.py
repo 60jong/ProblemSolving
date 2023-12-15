073e884f282433881193848d11dd19d332c89b78
60jong <rudwhd515@gmail.com>
2023-12-16 01:07:35 +0900

    1216

---
+++ solved_ac/BOJ/최소신장트리/최소스패닝트리_크루스칼.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+
+
+# union-find
+def get_parent(x):
+    if parent[x] == x:
+        return x
+    parent[x] = get_parent(parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    pa = get_parent(a)
+    pb = get_parent(b)
+
+    if pa < pb:
+        parent[pb] = pa
+    elif pa > pb:
+        parent[pa] = pb
+
+
+V, E = map(int, input().strip().split())
+
+edges = []
+parent = [i for i in range(V + 1)]
+
+# input
+for _ in range(E):
+    a, b, c = map(int, input().strip().split())
+    edges.append((c, a, b))
+
+# edge sorting
+edges = sorted(edges, key=lambda x: x[0], reverse=True)
+
+conn_edges = 0
+weights = 0
+while edges and conn_edges < (V - 1):  # V - 1 개 -> stop
+    cost, v1, v2 = edges.pop()
+    if get_parent[v1] != get_parent[v2]:
+        union(v1, v2)
+        conn_edges += 1
+        weights += cost
+
+print(weights)
